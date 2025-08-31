c3a0008c10abd8a5818f5e45bcd0321e36fa5092
LENAR <0x4C334E3438@users.noreply.github.com>
2025-08-31 23:31:31 +0800

    Update: 08-31-2025

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -12,7 +12,7 @@ src-git lite_watchdog https://github.com/4IceG/luci-app-lite-watchdog
 src-git my_pkg https://github.com/0x4C334E3438/My_PKG
 src-git modemband https://github.com/4IceG/luci-app-modemband
 src-git modemdefine https://github.com/4IceG/luci-app-modemdefine
-src-git sms_tool https://github.com/4IceG/luci-app-sms-tool
+src-git sms_tool_js https://github.com/4IceG/luci-app-sms-tool-js
 src-git xray https://github.com/yichya/luci-app-xray
 src-git v2raya https://github.com/v2rayA/v2raya-openwrt
 src-git zapret https://github.com/remittor/zapret-openwrt
